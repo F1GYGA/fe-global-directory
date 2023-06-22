b63f62009c4a072f15726019e2151af50eb1a38b
radudeacu <deacuradu@gmail.com>
2023-06-22 23:38:04 +0300

    integrated the forgot-password feature with the backend

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ForgotPasswordFormData } from '../../api/types/auth';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-forgot-password',
@@ -16,11 +18,32 @@ export class ForgotPasswordComponent {
     email: this.emailFormControl,
   });
 
+  constructor(private snackBar: MatSnackBar,
+    private http: HttpClient) {}
+
   onSubmit() {
     if (this.forgotPasswordForm.valid) {
       const forgotPasswordData: ForgotPasswordFormData = this.forgotPasswordForm
         .value as ForgotPasswordFormData;
       console.log(forgotPasswordData);
+
+      this.http.post('http://localhost:8080/sendEmail', { email: forgotPasswordData.email }, { responseType: 'text' }).subscribe(
+        (response) => {
+          console.log('Email sent successfully');
+        },
+        (error) => {
+          console.error('Failed to send email!', error);
+        }
+      );
+
+      this.snackBar.open(`Sending email to ${forgotPasswordData.email}`, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: 'custom-snackbar',
+    });
+
+      this.forgotPasswordForm.reset();
     }
   }
 }
