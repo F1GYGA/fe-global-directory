c26c933ff4e0125e062ad537ff0d686c8378c5f6
radudeacu <deacuradu@gmail.com>
2023-06-26 17:19:29 +0300

    about me now displays the current user's photo correctly

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,6 +3,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import { User } from 'src/api/types/user';
+import { UserService } from 'src/api/services/user/user.service';
 
 @Component({
   selector: 'app-about',
@@ -10,10 +12,24 @@ import {MatDividerModule} from '@angular/material/divider';
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  
+  currentUser!: User;
+  profilePhotoPlaceholder = 'path-to-placeholder-image.jpg';
+  
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+      this.userService.getUserById(Number(userId)).subscribe(
+        (user: User) => {
+          this.currentUser = user;
+        },
+        (error: any) => {
+          console.error('Failed to fetch user data:', error);
+        }
+      );
+    }
   }
 
   onNavigateToLinkedIn() {
